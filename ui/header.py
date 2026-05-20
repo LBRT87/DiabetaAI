dd306bf2e81d778a8f73e5c797922178e7731e06
Elbert Joan <elbertjoan877@gmail.com>
2026-05-20 15:10:38 +0700

    Fixed bug path logo

--- ui/header.py
+++ ui/header.py
@@ -3,7 +3,7 @@ import base64
 import streamlit as st
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-LOGO_PATH = os.path.join(BASE_DIR, "assets", "logo.png")
+LOGO_PATH = os.path.join(BASE_DIR, "assets", "Logo.png")
 
 
 def _img_to_b64(path: str):
